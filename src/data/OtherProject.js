ae1bc79f5974fc73b24a1937ed546071f129c7b2
phenyo-code <phenyo.koikoi@icloud.com>
2025-02-03 14:00:05 +0200

    updated data

--- src/data/OtherProject.js
+++ src/data/OtherProject.js
@@ -1,12 +1,12 @@
 const OtherProjects = [
     {
       id: 1,
-      image: 'https://images.pexels.com/photos/1735559/pexels-photo-1735559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      name: 'Flare - Full Stack E-commerce With Admin Dashboard (In Progress)',
-      language: 'Next.js, MongoDB',
+      image: 'https://images.unsplash.com/photo-1523398002811-999ca8dec234?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name: 'Flare - Full Stack E-commerce(In Progress) But You can Still Check It Out',
+      language: 'Next.js, MongoDB, TailwindCSS',
       githubLink: 'https://github.com/username/ecommerce-app',
       description: 'An e-commerce app built with React Native.',
-      url: 'https://no-website-url.com', 
+      url: 'https://flare-shop.vercel.app/', 
     },
     {
       id: 2,
--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,7 +1,16 @@
 // src/data/projects.js
 const FeaturedProjects = [
+  {
+    id: 1,
+    image: 'https://flare-shop.vercel.app/opengraph-image.png',
+  name: 'Flare - Full Stack E-commerce(In Progress) But You can Still Check It Out',
+  language: 'Next.js, MongoDB, TailwindCSS',
+  githubLink: 'https://github.com/username/ecommerce-app',
+  description: 'An e-commerce app built with React Native.',
+  url: 'https://flare-shop.vercel.app/',   // Project link
+  },
     {
-        id: 1,
+        id: 2,
         name: 'JobMatch - Compare your resume with a job description and receive feedback.',
         language: 'React.js',
         githubLink: 'https://github.com/phenyo-code/match-jobs.git',
@@ -9,7 +18,7 @@ const FeaturedProjects = [
         image: 'https://images.pexels.com/photos/4226115/pexels-photo-4226115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',  
       },
       {
-        id: 2,
+        id: 3,
         image: "https://cloudmatetechnologies.com/wp-content/uploads/2024/06/react.js.png",
         name: 'ReactLab - Learn React.js From Scratch With Advanced Concepts',
         language: 'Next.js, Tailwind CSS',
@@ -18,7 +27,7 @@ const FeaturedProjects = [
         url: 'https://reactlab-one.vercel.app',  // Project link
       },
       {
-        id: 3,
+        id: 4,
         image: 'https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         name: 'Roomify - Find Your Perfect Stay With A Wide Variety Of Accommodations.',
         language: 'Next.js, Tailwind CSS',
@@ -27,13 +36,13 @@ const FeaturedProjects = [
         url: 'https://roooomify.vercel.app/',
       },
       {
-        id: 4,
-        image: require('../assets/ProjectImages/share-image.png'),
-        name: 'DesignCraft - Web Development, App Development & Digital Marketing Services',
-        language: 'HTML, CSS, JavaScript',
-        githubLink: 'https://github.com/username/portfolio-website',
-        description: 'DesignCraft - Web Development, App Development & Digital Marketing Services',
-        url: 'https://designcrafts.netlify.app/',  // Project link
+        id: 1,
+        image: 'https://flare-shop.vercel.app/opengraph-image.png',
+      name: 'Flare - Full Stack E-commerce(In Progress) But You can Still Check It Out',
+      language: 'Next.js, MongoDB, TailwindCSS',
+      githubLink: 'https://github.com/username/ecommerce-app',
+      description: 'An e-commerce app built with React Native.',
+      url: 'https://flare-shop.vercel.app/',   // Project link
       },
   ];
   
